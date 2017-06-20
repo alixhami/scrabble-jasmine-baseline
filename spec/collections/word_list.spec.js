dff46805bd71f7b64781271b83bcde6006d5e3f6
Alix Hamilton <alix.hamilton@gmail.com>
2017-06-20 12:21:11 -0700

    completes word list functions and tests

--- spec/collections/word_list.spec.js
+++ spec/collections/word_list.spec.js
@@ -1,29 +1,61 @@
 // Do not remove
+import Word from 'models/word';
 import WordList from 'collections/word_list';
 
 describe('WordList', function() {
   describe('highestScoringWord', function() {
-    // TODO: fill these out
+
     it ('Returns undefined if there are no words', function() {
+      var wordList = new WordList();
+      expect(wordList.highestScoringWord()).toEqual(undefined);
     });
 
     it ('If there is only one word, return it', function() {
+      var word = new Word({ text: 'bacon' });
+      var wordList = new WordList(word);
+      expect(wordList.highestScoringWord()).toEqual(word);
     });
 
     it ('Returns the highest word if there are two words', function() {
+      var word1 = new Word({ text: 'bacon' });
+      var word2 = new Word({ text: 'twelve' });
+      var wordList = new WordList([word1, word2]);
+
+      expect(wordList.highestScoringWord()).toEqual(word2);
     });
 
     it ('If tied, prefer a word with 7 letters', function() {
+      var word1 = new Word({ text: 'zzzzzz' });
+      var word2 = new Word({ text: 'virtual' });
+      var wordList = new WordList([word1, word2]);
+
+      expect(wordList.highestScoringWord()).toEqual(word2);
     });
 
     it ('If tied and no word has 7 letters, prefers the word with fewer letters', function() {
     });
 
     it ('Returns the first word of a tie with same letter count', function() {
+      var word1 = new Word({ text: 'bacon' });
+      var word2 = new Word({ text: 'sandy' });
+      var wordList = new WordList([word1, word2]);
+
+      expect(wordList.highestScoringWord()).toEqual(word1);
     });
   });
 
   describe('totalScore', function() {
-    // TODO - what interesting cases are there?
+    it ('Returns the total score of two valid words', function () {
+      var word1 = new Word({ text: 'bacon' });
+      var word2 = new Word({ text: 'sandy' });
+      var wordList = new WordList([word1, word2]);
+
+      expect(wordList.totalScore()).toEqual(18);
+    });
+
+    it ('Returns zero if there are no words', function () {
+      var wordList = new WordList();
+      expect(wordList.totalScore()).toEqual(0);
+    });
   });
 });
--- src/collections/word_list.js
+++ src/collections/word_list.js
@@ -8,13 +8,42 @@ const WordList = Backbone.Collection.extend({
   // Returns the Word model with the highest score, according
   // to standard tie-breaking rules
   highestScoringWord: function() {
-    // TODO: test and implement
-    return this.at(-1);
+    if (this.length <= 1) {
+      return this.at(-1);
+    }
+
+    // builds array of highest scoring words
+    var highestScoringWords = [];
+    var highestScore = 0;
+    this.each(function (word) {
+      if (word.score() === highestScore) {
+        highestScoringWords.push(word);
+      } else if (word.score() > highestScore) {
+        highestScoringWords = [ word ];
+        highestScore = word.score();
+      }
+    });
+
+    // tie breaker
+    var highestScoringWord = highestScoringWords[0];
+    highestScoringWords.forEach(function (word) {
+      var currentWord = word.get('text');
+      if (currentWord.length < highestScoringWord.length && highestScoringWord.length != 7) {
+        highestScoringWord = word;
+      } else if (currentWord.length === 7 && highestScoringWord.length != 7) {
+        highestScoringWord = word;
+      }
+    });
+    return highestScoringWord;
   },
 
   totalScore: function() {
-    // TODO: test and implement
-    return 0;
+    var total = 0;
+
+    this.each(function (word) {
+      total += word.score();
+    });
+    return total;
   }
 });
 
